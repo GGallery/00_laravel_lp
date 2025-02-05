3fdf34ec0767f331f9409768d15fb0940f4a45fa
lucapolzone <lucapolzone@gmail.com>
2025-02-05 16:39:09 +0100

    add rate limit ma manca la cache nel dn

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QuestionController;
 
 Route::get('/', [QuestionController::class, 'index'])->name('index');
-Route::post('/submit-answers', [GuestController::class, 'submitAnswers'])->name('submitAnswers'); // invia le risposte del questionario
+
+Route::post('/submit-answers', [GuestController::class, 'submitAnswers'])->middleware('throttle:submit-answers')->name('submitAnswers');  // invia le risposte del questionario. il middleware throttle limita il numero di richieste per IP (Rate limit).
+
 Route::get('/result', [GuestController::class, 'result'])->name('result'); // Le visualizza
+
